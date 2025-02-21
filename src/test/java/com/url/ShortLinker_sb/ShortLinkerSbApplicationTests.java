b6b8b702388d5d5b321d71e63f849b0dc3f37c6b
Lahcen ELORF <lahcenelorf.24@gmail.com>
2025-02-21 19:35:39 +0100

    Second commit

---
+++ src/test/java/com/url/ShortLinker_sb/ShortLinkerSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.url.ShortLinker_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortLinkerSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
